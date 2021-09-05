c6c58491561a5b94dfa7727a5c3192743a024e12
Atul Saini <atulsaini9886@gmail.com>
2021-09-05 15:01:09 +0530

    Added firebase and redux

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAbzy2bVB5qD5nbIFL-Z6dJAssbB_WLGvM",
+  authDomain: "slack-clone-22-01.firebaseapp.com",
+  projectId: "slack-clone-22-01",
+  storageBucket: "slack-clone-22-01.appspot.com",
+  messagingSenderId: "847157135793",
+  appId: "1:847157135793:web:15b60a7b2f72f05f92d590",
+  measurementId: "G-TL3N9MFKGC",
+};
